ebebd861b2f9c0ad8a5be17031e4e6d0c7266686
Casé <72296275+casemonster1@users.noreply.github.com>
2025-06-11 21:49:05 -0300

    Update main.py

--- sistema_estoque_backend/src/main.py
+++ sistema_estoque_backend/src/main.py
@@ -47,7 +47,7 @@ def home():
 with app.app_context():
     db.create_all()
 
-if __name__ == \'__main__\':
+if __name__ == '__main__':
     # Use a porta fornecida pelo Render (PORT) ou 5001 para desenvolvimento local
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
